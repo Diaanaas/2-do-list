610dfdf266e069b9f39ce26120c7ea9ccd5526f3
Diaanaas <d1877087@gmail.com>
2023-10-24 02:46:29 +0300

    Rework

--- src/App.js
+++ src/App.js
@@ -1,140 +1,123 @@
-import './App.css';
-import Button from './components/Button';
-import { useState } from 'react';
-import TodoList from './components/TodoList';
+import React, { useState, useEffect } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
-let nextId = 4;
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+  const [editingTodo, setEditingTodo] = useState(null);
+  const [editedText, setEditedText] = useState('');
 
-let initialTodos = [
-  {
-    id: 0,
-    title: 'Learn HTML+CSS',
-    done: true
-  },
-  {
-    id: 1,
-    title: 'Learn Javascript+React',
-    done: true
-  },
-  {
-    id: 2,
-    title: 'Write dynamic UI frontend',
-    done: true
-  },
-  {
-    id: 3,
-    title: 'Get points for the task',
-    done: false
-  }
-];
-
-function App() {
-  let localstorageTodos = [];
-  if (localStorage.getItem('allTodos')) {
-    JSON.parse(localStorage.getItem('allTodos')).forEach(todo => {
-      localstorageTodos.push(todo);
-    });
-  }
-  else {
-    localstorageTodos = null;
-  }
-  initialTodos = localstorageTodos || initialTodos;
-
-  function sortTodos(todos) {
-    todos.sort((a, b) => {
-      if (a.id < b.id) {
-        return -1;
-      }
-      else if (a.id > b.id) {
-        return 1;
-      }
-      else {
-        return 0;
-      }
-    });
-  }
-  sortTodos(initialTodos);
-  nextId = localStorage.getItem('nextId') || nextId;
-  //localStorage.clear();
-  const [todos, setTodos] = useState(initialTodos);
-
-  function swapTodos(id1, id2) {
-    let resTodos = [...todos];
-
-    let i1 = resTodos.findIndex(x => x.id == id1);
-    let i2 = resTodos.findIndex(x => x.id == id2);
-
-    const id = resTodos[i1].id;
-    resTodos[i1].id = resTodos[i2].id;
-    resTodos[i2].id = id;
-
-    sortTodos(resTodos);
-
-    setTodos(
-      resTodos
-    );
-    localStorage.setItem('allTodos', JSON.stringify(resTodos));
-  }
-
-  function handleAddTodo(e) {
-    e.preventDefault();
-    const form = e.target;
-    const formData = new FormData(form);
-    const formJson = Object.fromEntries(formData.entries());
-
-    console.log(formJson);
-
-    if (formJson.todoText === '' || !formJson.todoText) {
-      formJson.todoText = 'Do something';
+  useEffect(() => {
+    const storedTodos = [];
+    if (localStorage.getItem('data')) {
+      JSON.parse(localStorage.getItem('data')).forEach(todo => {
+        storedTodos.push(todo);
+      });
     }
-    let resTodos = [
-      ...todos,
-      {
-        id: nextId++,
-        title: formJson.todoText,
-        done: false
+    setTodos(storedTodos);
+  }, []);
+
+  const addTodo = () => {
+    if (newTodo) {
+      const newTodoItem = {
+        id: Date.now(),
+        text: newTodo,
+      };
+      setTodos([...todos, newTodoItem]);
+      localStorage.setItem('data', JSON.stringify([...todos, newTodoItem]));
+      setNewTodo('');
+    }
+  };
+
+  const deleteTodo = (id) => {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    localStorage.setItem('data', JSON.stringify(updatedTodos));
+    setTodos(updatedTodos);
+  };
+
+  const startEditing = (id, text) => {
+    setEditingTodo(id);
+    setEditedText(text);
+  };
+
+  const saveEditedTodo = (id) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, text: editedText };
       }
-    ]
-    setTodos(
-      resTodos
-    );
-    localStorage.setItem('allTodos', JSON.stringify(resTodos));
-    localStorage.setItem('nextId', nextId);
-  }
+      return todo;
+    });
+    setTodos(updatedTodos);
+    localStorage.setItem('data', JSON.stringify(updatedTodos));
+    setEditingTodo(null);
+  };
 
-  function handleChangeTodo(todo) {
-    const index = todos.findIndex(t => t.id === todo.id);
-    todo.done = !todo.done;
+  const handleDragEnd = (result) => {
+    if (!result.destination) return;
 
-    todos[index].done = todo.done;
-    setTodos(todos);
-    localStorage.setItem('allTodos', JSON.stringify(todos));
-  }
+    const reorderedTodos = Array.from(todos);
+    const [reorderedItem] = reorderedTodos.splice(result.source.index, 1);
+    reorderedTodos.splice(result.destination.index, 0, reorderedItem);
+    localStorage.setItem('data', JSON.stringify(reorderedTodos));
 
-  function handleDeleteTodo(todoId) {
-    var remainingTodos = todos.filter(t => t.id !== todoId);
-    setTodos(
-      remainingTodos
-    );
-    localStorage.setItem('allTodos', JSON.stringify(remainingTodos));
-  }
+    setTodos(reorderedTodos);
+  };
 
   return (
-    <div className="App">
-      <div className='wrapper'>
-        <header className="App-header">
-          <Button
-            handleSubmit={handleAddTodo}
-          />
-        </header>
-
-        <TodoList todos={todos}
-          onDelete={handleDeleteTodo}
-          swapTodos={swapTodos}
-          onChange={handleChangeTodo}/>
+    <div className="App" style={{ width: "250px" }}>
+      <h1>Todo List</h1>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}>Add</button>
       </div>
+      <div style={{ height: "30px" }}></div>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableId="todos">
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {todos.map((todo, index) => (
+                <Draggable key={todo.id} draggableId={todo.id.toString()} index={index}>
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <div>
+                        {editingTodo === todo.id ? (
+                          <>
+                            <input
+                              type="text"
+                              value={editedText}
+                              onChange={(e) => setEditedText(e.target.value)}
+                            />
+                            <button onClick={() => saveEditedTodo(todo.id)}>Save</button>
+                          </>
+                        ) : (
+                          <>
+                            <span>{todo.text}</span>
+                            <button onClick={() => startEditing(todo.id, todo.text)}>
+                              Edit
+                            </button>
+                          </>
+                        )}
+                        <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+                      </div>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
-}
+};
 
 export default App;
